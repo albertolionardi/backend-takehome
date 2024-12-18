eef6861c0d4c90505413a4b4e030202cc7de80a3
Alberto <albertolionardi1@gmail.com>
2024-12-18 06:51:48 +0100

    complete post structs

--- app/pkg/models/posts.go
+++ app/pkg/models/posts.go
@@ -1,56 +1,33 @@
 package models
 
 import (
-	"app/pkg/config"
+	"log"
+	"time"
 
-	"github.com/jinzhu/gorm"
+	"gorm.io/gorm"
 )
 
-var db *gorm.DB
-
 type Post struct {
-	gorm.Model
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	AuthorID  int       `json:"author_id"`
-	CreatedAt string    `json:"created_at"`
-	UpdatedAt string    `json:"updated_at"`
-	Comments  []Comment `json:"comments"`
+	ID        uint `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	AuthorID  uint   `json:"author_id"`
 }
 
-func init() {
-	config.Connect()
-	db = config.GetDB()
+type CreatePost struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+type UpdatePost struct {
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	UpdatedAt time.Time
+}
+
+func MigratePost(db *gorm.DB) {
 	db.AutoMigrate(&Post{})
-}
-
-func (p *Post) CreatePost() *Post {
-	db.NewRecord(p)
-	db.Create(&p)
-	return p
-}
-
-func GetPosts() []Post {
-	var Posts []Post
-	db.Find(&Posts)
-	return Posts
-}
-
-func GetPostById(ID int64) (*Post, *gorm.DB) {
-	var getPost Post
-	db := db.Where("ID=?", ID).Find(&getPost)
-	return &getPost, db
-}
-
-func DeletePost(ID int64) Post {
-	var post Post
-	db.Where("ID=?", ID).Delete(post)
-	return post
-}
-
-// Update Post ?
-func UpdatePost(ID int64) Post {
-	var post Post
-	db.Where("ID=?", ID).Update(post)
-	return post
+	log.Println("Post Table Migration Completed...")
 }
