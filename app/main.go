47a3c2c16157dbeb9edc7c5bb0b910ee67986b65
Alberto <albertolionardi1@gmail.com>
2024-12-18 06:50:10 +0100

    add auto migration for models, and add routes to main

--- app/main.go
+++ app/main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"app/pkg/config"
+	"app/pkg/models"
+	"app/pkg/routes"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	config.Connect()
-	fmt.Println("Server is running on222 http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	db := config.Connect()
+	models.MigrateComment(db)
+	models.MigratePost(db)
+	models.MigrateUser(db)
+	models.MigrateSession(db)
 
+	router := mux.NewRouter().StrictSlash(true)
+	routes.BlogRoutes(router, db)
+
+	fmt.Println("Server is running on222 http://localhost:8080")
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
