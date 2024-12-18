fc0fd2d1357d2a498e5e0a97398d8daf981e9e03
Alberto <albertolionardi1@gmail.com>
2024-12-18 06:58:57 +0100

    change BlogRoutes into a function and add db to the parameter for more flexibility

--- app/pkg/routes/blog-routes.go
+++ app/pkg/routes/blog-routes.go
@@ -1,21 +1,23 @@
 package routes
 
 import (
-	"app/pkg/controllers"
-
 	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 )
 
-var BlogRoutes = func(router *mux.Router) {
-	router.HandleFunc("/register/", controllers.Register).Methods("POST")
-	router.HandleFunc("/login/", controllers.Login).Methods("POST")
+func BlogRoutes(router *mux.Router, db *gorm.DB) {
+	controllers := NewController(db)
+	router.HandleFunc("/register", controllers.Register).Methods("POST")
 
-	router.HandleFunc("/posts/", controllers.CreatePost).Methods("POST")
+	router.HandleFunc("/login", controllers.Login).Methods("POST")
+
+	router.HandleFunc("/posts", controllers.CreatePost).Methods("POST")
 	router.HandleFunc("/posts/{id}", controllers.GetPostById).Methods("GET")
-	router.HandleFunc("/posts/", controllers.GetPosts).Methods("GET")
+	router.HandleFunc("/posts", controllers.GetPosts).Methods("GET")
 	router.HandleFunc("/posts/{id}", controllers.DeletePostById).Methods("DELETE")
+	router.HandleFunc("/posts/{id}", controllers.UpdatePost).Methods("PUT")
 
-	router.HandleFunc("/posts/{id}/comments/", controllers.PostComment).Methods("POST")
-	router.HandleFunc("/posts/{id}/comments/", controllers.ListAllComments).Methods("GET")
+	router.HandleFunc("/posts/{id}/comments", controllers.PostComment).Methods("POST")
+	router.HandleFunc("/posts/{id}/comments", controllers.ListAllComments).Methods("GET")
 
 }
