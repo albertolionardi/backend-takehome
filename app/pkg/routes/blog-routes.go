afbefdcdb2cedce986c9ea66e14b4aa6df2dcd55
Alberto <albertolionardi1@gmail.com>
2024-12-19 08:04:10 +0100

    separaate public routes and protected routes for middleware

--- app/pkg/routes/blog-routes.go
+++ app/pkg/routes/blog-routes.go
@@ -1,23 +1,30 @@
 package routes
 
 import (
+	"app/middleware"
+
 	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
 func BlogRoutes(router *mux.Router, db *gorm.DB) {
 	controllers := NewController(db)
+	sessionMiddleware := middleware.SessionMiddleware(db)
+	// Public routes
 	router.HandleFunc("/register", controllers.Register).Methods("POST")
-
 	router.HandleFunc("/login", controllers.Login).Methods("POST")
 
-	router.HandleFunc("/posts", controllers.CreatePost).Methods("POST")
-	router.HandleFunc("/posts/{id}", controllers.GetPostById).Methods("GET")
-	router.HandleFunc("/posts", controllers.GetPosts).Methods("GET")
-	router.HandleFunc("/posts/{id}", controllers.DeletePostById).Methods("DELETE")
-	router.HandleFunc("/posts/{id}", controllers.UpdatePost).Methods("PUT")
+	// Routes for middleware
+	proctedtedRouter := router.PathPrefix("/").Subrouter()
+	proctedtedRouter.Use(sessionMiddleware)
 
-	router.HandleFunc("/posts/{id}/comments", controllers.CreateComment).Methods("POST")
-	router.HandleFunc("/posts/{id}/comments", controllers.ListAllComments).Methods("GET")
+	proctedtedRouter.HandleFunc("/posts", controllers.CreatePost).Methods("POST")
+	proctedtedRouter.HandleFunc("/posts/{id}", controllers.GetPostById).Methods("GET")
+	proctedtedRouter.HandleFunc("/posts", controllers.GetPosts).Methods("GET")
+	proctedtedRouter.HandleFunc("/posts/{id}", controllers.DeletePostById).Methods("DELETE")
+	proctedtedRouter.HandleFunc("/posts/{id}", controllers.UpdatePost).Methods("PUT")
+
+	proctedtedRouter.HandleFunc("/posts/{id}/comments", controllers.CreateComment).Methods("POST")
+	proctedtedRouter.HandleFunc("/posts/{id}/comments", controllers.ListAllComments).Methods("GET")
 
 }
